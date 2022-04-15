5765b6765e81efcf20ef7b239aeaefe32f23ed18
Jhunrel Evangelista <91590771+Jhunrel25@users.noreply.github.com>
2022-04-15 21:38:52 +0800

    Create Calculator.java
    Author: Jhunrel Evangelista
    Twitter: @JhunrelE
    
    About:
    A simple calculator program that could solve very large numbers in precise presentation and could solve multiple operations at once.

---
+++ Calculator.java
@@ -0,0 +1,160 @@
+package com.jhunrel_evangelista.github.calcapp;
+import java.math.BigDecimal;
+
+public class Calculator {
+
+	private String[] tokens;
+	private int size;
+
+	public Calculator(String expression) {
+		this.tokens = this.addSpaces(expression).split("\\s");
+		this.size = this.tokens.length;
+	}
+
+
+	private String addSpaces(String expression) {
+		return
+			expression
+			.replaceAll("(?<=[\\d])(?=[\\^*/+-])", " ")
+			.replaceAll("(?<=[\\^*/+-])(?=[\\d])", " ")
+			.replaceAll("(?<=[)])(?=[\\^*/+-])", " ")
+			.replaceAll("(?<=[\\^*/+-])(?=[(])", " ")
+			.replaceAll("(?<=[(])(?=[\\d])", " ")
+			.replaceAll("(?<=[\\d])(?=[)])", " ");
+	}
+
+
+	private void removeAToken(int index) {
+		while (index < (this.size - 1))
+			this.tokens[ index ] = this.tokens[ 1 + (index++) ];
+
+		this.size--;
+	}
+
+
+	private void removeAlreadyUsedTokens(int index) {
+		for (byte i = 0; (i < 2); ++i)
+			this.removeAToken(index);
+	}
+
+
+	private void addComputationValueOfTokens(int index, String value) {
+		this.tokens[ index ] = value;
+	}
+
+
+	private void parenthesis(int index) {
+		StringBuilder parenthesisValue = new StringBuilder();
+
+		while (!(this.tokens[ index + 1 ].equals(")"))) {
+			parenthesisValue
+			.append(this.tokens[ index + 1 ])
+			.append(" ");
+
+			this.removeAToken(index + 1);
+		}
+		this.removeAToken(index + 1);
+
+		this.addComputationValueOfTokens(
+			(index),
+
+			new Calculator(parenthesisValue.toString())
+			.equals()
+		);
+	}
+
+
+	private void exponents(int index) {
+		String value = new BigDecimal(this.tokens[ index - 1 ])
+
+		.pow(Integer.parseInt(this.tokens[ index + 1 ]))
+		.toString();
+
+		this.addComputationValueOfTokens((index - 1), (value));
+		this.removeAlreadyUsedTokens(index);
+	}
+
+
+	private void multiplication(int index) {
+		String value = new BigDecimal(this.tokens[ index - 1 ])
+
+		.multiply(new BigDecimal(this.tokens[ index + 1 ]))
+		.toString();
+
+		this.addComputationValueOfTokens((index - 1), (value));
+		this.removeAlreadyUsedTokens(index);
+	}
+
+
+	private void division(int index) {
+		String value = new BigDecimal(this.tokens[ index - 1 ])
+
+		.divide(new BigDecimal(this.tokens[ index + 1 ]))
+		.toString();
+
+		this.addComputationValueOfTokens((index - 1), (value));
+		this.removeAlreadyUsedTokens(index);
+	}
+
+
+	private void addition(int index) {
+		String value = new BigDecimal(this.tokens[ index - 1 ])
+
+		.add(new BigDecimal(this.tokens[ index + 1 ]))
+		.toString();
+
+		this.addComputationValueOfTokens((index - 1), (value));
+		this.removeAlreadyUsedTokens(index);
+	}
+
+
+	private void subtraction(int index) {
+		String value = new BigDecimal(this.tokens[ index - 1 ])
+
+		.subtract(new BigDecimal(this.tokens[ index + 1 ]))
+		.toString();
+
+		this.addComputationValueOfTokens((index - 1), (value));
+		this.removeAlreadyUsedTokens(index);
+	}
+
+
+	private String orderOfOperation() {
+		if (this.size == 0)
+			return null;
+
+		if (this.size == 1)
+			return this.tokens[ 0 ];
+
+		for (int i = 0; (i < this.size); ++i)
+			if (this.tokens[ i ].equals("("))
+				this.parenthesis(i--);
+
+		for (int i = 0; (i < this.size); ++i)
+			if (this.tokens[ i ].equals("^"))
+				this.exponents(i--);
+
+		for (int i = 0; (i < this.size); ++i)
+			if (this.tokens[ i ].equals("*"))
+				this.multiplication(i--);
+
+		for (int i = 0; (i < this.size); ++i)
+			if (this.tokens[ i ].equals("/"))
+				this.division(i--);
+
+		for (int i = 0; (i < this.size); ++i)
+			if (this.tokens[ i ].equals("+"))
+				this.addition(i--);
+
+		for (int i = 0; (i < this.size); ++i)
+			if (this.tokens[ i ].equals("-"))
+				this.subtraction(i--);
+
+		return this.tokens[ 0 ];
+	}
+
+
+	public String equals() {
+		return this.orderOfOperation();
+	}
+}
